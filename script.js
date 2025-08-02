5ae5869c515196ada20acf021ffd8a4a3a7f692d
Tanko Fabrice Ngala <tankofabrice4@gmail.com>
2025-08-02 20:46:42 +0100

    Form Validation

---
+++ script.js
@@ -0,0 +1,47 @@
+// form valitation
+
+document.assEventListener(` DOMContentLoaded`, function(){
+    const form = document.getElementById(`registration-form`);
+    const feedbackDiv = document.getElementById(`form-feedback`);
+
+    form.addEventListener(`submit`, function(event){
+        event.preventDefault();
+
+        const usernameInput = document.getElementById(`username`);
+        const emailInput = document.getElementById(`email`);
+        const passwordInput = document.getElementById(`password`);
+
+        const username = usernameInput.value.trim();
+        const email = emailInput.value.trim();
+        const password = passwordInput.value.trim();
+
+        let isValid = true;
+        let messages = [];
+
+        // validate username
+        if(username.length < 3){
+            isValid = false;
+            messages.push(`Uaername must be atleast 3 characters long`);
+        }
+        // email validation
+        if(!email.includes(`@`)) || !email.includes(`.`){
+            isValid = false;
+            messages.push(`Email is invalid`);
+        }
+        // password validation
+        if(password.length >= 8){
+            isValid = false;
+            messages.push(`password must be at least 8 characters`);
+        }
+
+        // display outputs
+        feedbackDiv.style.display = "block"
+        if(isValid){
+            feedbackDiv.textContent("Registration successful!");
+            feedbackDiv.style.color = "#28a745";
+        } else {
+             feedbackDiv.innerHTML = messages.join("<br>");
+             feedbackDiv.style.color = "#dc3545";
+        }
+    });
+});
